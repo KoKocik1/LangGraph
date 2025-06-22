d02e01be6ac5e47558046c8ff8e77cf2571e9319
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-06-22 22:38:24 +0200

    add agentic-rag project

---
+++ agentic-rag/main.py
@@ -0,0 +1,9 @@
+from graph.graph import app
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    print("Hello Advanced RAG")
+    # print(app.invoke(input={"question": "agent memory?"}))
